b2bf3829927de8c7c663bb0ca540a303d336765f
Roberto Nogueira <enogrob@gmail.com>
2017-12-27 16:14:51 -0200

    updated-todayrc-install

--- .todayrc_install.sh
+++ .todayrc_install.sh
@@ -2,7 +2,7 @@
 ## Crafted (c) 2013~2017 by ZoatWorks Software LTDA.
 ## Prepared : Roberto Nogueira
 ## File     : .todayrc_install.sh
-## Version  : PA02
+## Version  : PA03
 ## Date     : 2018-12-27
 ## Project  : project-things-today
 ## Reference: bash
@@ -27,7 +27,7 @@ if [ $OS == "Linux" ]; then
     mkdir -p "$HOME/Books"
     chown enogrob:enogrob "$HOME/Books"
     touch $HOME/Books/My_Kindle_Content
-    touch $HOME/Books/Calibre\ Library
+    ln -sf "$HOME/Books/Calibre Library" "Calibre Library"
   fi
   if [ ! -d "/Volumes/Data HD" ]; then
       mkdir -p /Volumes/Data\ HD
@@ -42,34 +42,44 @@ fi
 
 if [ ! -d "$HOME/Things" ]; then
   cp -rf ./Things "$HOME"
+  chown enogrob:enogrob $HOME/Things
   ln -sf $LOCAL $HOME/THINGS_HOME
 else
   if [ ! -L $HOME/Things/Areas ] && [ ! -d $HOME/Things/Areas ]; then
     cp -rf ./Things/Areas $HOME/Things/Areas
+    chown enogrob:enogrob $HOME/Things/Areas
   fi
   if [ ! -L $HOME/Things/Inbox ] && [ ! -d $HOME/Things/Inbox ]; then
     cp -rf ./Things/Inbox $HOME/Things/Inbox
+    chown enogrob:enogrob $HOME/Things/Inbox
   fi
   if [ ! -L $HOME/Things/Logbook ] && [ ! -d $HOME/Things/Logbook ]; then
     cp -rf ./Things/Logbook $HOME/Things/Logbook
+    chown enogrob:enogrob $HOME/Things/Logbook
   fi
   if [ ! -L $HOME/Things/Projects ] && [ ! -d $HOME/Things/Projects ]; then
     cp -rf ./Things/Projects $HOME/Things/Projects
+    chown enogrob:enogrob $HOME/Things/Projects
   fi
   if [ ! -L $HOME/Things/Resources ] && [ ! -d $HOME/Things/Resources ]; then
     cp -rf ./Things/Resources $HOME/Things/Resources
+    chown enogrob:enogrob $HOME/Things/Resources
   fi
   if [ ! -L $HOME/Things/Scheduled ] && [ ! -d $HOME/Things/Scheduled ]; then
     cp -rf ./Things/Scheduled $HOME/Things/Scheduled
+    chown enogrob:enogrob $HOME/Things/Scheduled
   fi
   if [ ! -L $HOME/Things/Someday ] && [ ! -d $HOME/Things/Someday ]; then
     cp -rf ./Things/Someday $HOME/Things/Someday
+    chown enogrob:enogrob $HOME/Things/Someday
   fi
   if [ ! -L $HOME/Things/SomedayLog ] && [ ! -d $HOME/Things/SomedayLog ]; then
     cp -rf ./Things/SomedayLog $HOME/Things/SomedayLog
+    chown enogrob:enogrob $HOME/Things/SomedayLog
   fi
   if [ ! -L $HOME/Things/Trash ] && [ ! -d $HOME/Things/Trash ]; then
     cp -rf ./Things/Trash $HOME/Things/Trash
+    chown enogrob:enogrob $HOME/Things/Trash
   fi
 fi
 
