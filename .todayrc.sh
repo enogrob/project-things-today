1b52cef4b9f8d0b94d30a50ca0c15121a2208968
Roberto Nogueira <enogrob@gmail.com>
2019-10-30 10:29:14 -0300

    add-sveltejs-context

--- .todayrc.sh
+++ .todayrc.sh
@@ -2,8 +2,8 @@
 ## Crafted (c) 2013~2019 by ZoatWorks Software LTDA.
 ## Prepared : Roberto Nogueira
 ## File     : .todayrc.sh
-## Version  : PA79
-## Date     : 2019-08-29
+## Version  : PA80
+## Date     : 2019-10-30
 ## Project  : project-things-today
 ## Reference: bash
 ##
@@ -17,7 +17,7 @@ source ~/.todayrc_vars.sh
 shopt -s extglob
 
 TAGSPROJECTS='+(aws|chrome|codewars|conf|coursera|ebook|edx|exercism|futurelearn|graphacademy|hackerrank|javabrains|job|krishnamurti|lab360|linkedin|linuxacademy|oreilly|phoenix|pragmaticstudio|project|rails|sololearn|specialization|tutorial|tutorialspoint|udemy|jetbrains)'
-TAGSCONTEXTS='+(bash|bigdata|bluemix|bootstrap|chartjs|cpp|css|delphi|design|devops|elixir|elm|erlang|grails|groovy|hadoop|html|java|javascript|jekyll|jquery|nodejs|neo4j|phoenix|python|r|rails|reactjs|rspec|ruby|spring|sinatra|springboot|sql|unix|vim|vuejs|webpack)'
+TAGSCONTEXTS='+(bash|bigdata|bluemix|bootstrap|chartjs|cpp|css|delphi|design|devops|elixir|elm|erlang|grails|groovy|hadoop|html|java|javascript|jekyll|jquery|nodejs|neo4j|phoenix|python|r|rails|reactjs|rspec|ruby|spring|sinatra|springboot|sql|sveltejs|unix|vim|vuejs|webpack)'
 
 OS=`uname`
 if [ $OS == "Linux" ]; then
